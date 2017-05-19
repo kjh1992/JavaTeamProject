e0582caa270428c62f1d6d4739d19dc7bafecd5e
kjh1992 <wer2wer306@gmail.com>
2017-05-19 13:59:50 +0900

    df

---
+++ src/teamprojcet/ClassInfor.java
@@ -0,0 +1,12 @@
+package teamprojcet;
+
+public class ClassInfor {
+
+	private StringToken st; // 파싱클래스
+	private KeyWord kw = new KeyWord();
+	private String[] classContent;	// 클래스내용
+	private String classTitle;	// 클래스 이름
+	private int num_of_method;	// 메소드 개수
+	private int num_of_member;	// 멤버 개수
+
+}
---
+++ src/teamprojcet/MemberData.java
@@ -0,0 +1,5 @@
+package teamprojcet;
+
+public class MemberData {
+
+}
